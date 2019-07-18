6b5ab48549152833a639b6ade65d668ef1f5950d
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-19 07:06:18 +0900

    [update] 諸々実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,19 @@ $(function(){
   // 画像の高さを毎回計算
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-  console.log(document.documentElement.style.setProperty('--vh', `${vh}px`));
 
-  $('.scroll').click(function(){
+  $('.scroll').click(function() {
     var speed = 500;
     var position = $(this).parent().next().offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
   });
+
+  // ページを離れる時の警告を出す
+  window.onbeforeunload = function(e) {
+    if($('#form-target').length) {
+      return "このページを離れますか？";
+      e.returnValue = "このページを離れますか？";
+    }
+  }
 });
