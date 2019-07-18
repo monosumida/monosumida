6fdff8a37cde62d5c4c611108eb8fcc8719008f9
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-18 22:45:00 +0900

    [add] create top page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -17,3 +5,16 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+$(function(){
+  // 画像の高さを毎回計算
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  console.log(document.documentElement.style.setProperty('--vh', `${vh}px`));
+
+  $('.scroll').click(function(){
+    var speed = 500;
+    var position = $(this).parent().next().offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
