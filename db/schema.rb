04f781662d7c285be68840ac0fa0457f684a3da2
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-19 00:08:47 +0900

    [add] ブログ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_150356) do
+ActiveRecord::Schema.define(version: 2019_07_18_150632) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
