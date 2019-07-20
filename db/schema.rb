4b17b5e9e10cce454860b198cabb839bb482d610
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-20 11:58:30 +0900

    [update] add active record association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_181243) do
+ActiveRecord::Schema.define(version: 2019_07_20_023456) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
+    t.integer "user_id"
   end
 
   create_table "topics", force: :cascade do |t|
