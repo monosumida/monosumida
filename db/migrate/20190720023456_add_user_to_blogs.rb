eed84efbb9cfef4db8242c98c581888b152538a5
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-20 12:45:58 +0900

    [update] change order column

--- db/migrate/20190720023456_add_user_to_blogs.rb
+++ db/migrate/20190720023456_add_user_to_blogs.rb
@@ -1,5 +1,5 @@
 class AddUserToBlogs < ActiveRecord::Migration[5.2]
   def change
-    add_reference :blogs, :user, foreign_key: true, index: true, after: :id
+    add_column :blogs, :user_id, :integer, foreign_key: true, index: true, after: :id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 2019_07_20_023456) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "content"
-    t.integer "user_id"
   end
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
