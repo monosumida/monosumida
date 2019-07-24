42b35cc5f384f388bd57f928f65fc533073bcf7f
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-24 10:53:54 +0900

    [update] 諸々デザイン修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :topics
-  resources :blogs
+  resources :blogs do
+    member do
+      post 'preview'
+    end
+  end
   namespace :admin do
     resources :sessions
   end
