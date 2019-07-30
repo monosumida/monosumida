db8dadea1fc0883e6d45dc73b6577372b14cde67
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-30 21:14:53 +0900

    [add] 管理画面にユーザを追加

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,43 @@
+class Admin::UsersController < Admin::ApplicationController
+  before_action :set_user, only: %i[edit update destroy]
+  protect_from_forgery except: [:preview]
+
+  def index
+    @users = User.all
+  end
+
+  def edit;end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(user_param_items)
+    end
+
+    def user_param_items
+      %i[public_flag]
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :sessions
     resources :blogs, only: %i[index edit update destroy]
+    resources :users, only: %i[index edit update destroy]
     root 'dashboard#index'
   end
   root 'home#top'
