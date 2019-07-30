55612cea70667ac8c5ac524e2f0381e8958a4db3
dj-hirrot <hirotoshibutani@gmail.com>
2019-07-30 20:26:12 +0900

    [update] スキーマの調整

--- db/migrate/20190718174321_create_blogs.rb
+++ db/migrate/20190718174321_create_blogs.rb
@@ -1,7 +1,10 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
+      t.integer :user_id, foreign_key: true, index: true
       t.string :title
+      t.text :content
+      t.bool :public_flag
 
       t.timestamps
     end
--- db/migrate/20190725124702_add_publish_at_to_blogs.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishAtToBlogs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :blogs, :publish_at, :datetime, after: :updated_at
-  end
-end
