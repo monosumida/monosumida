8b205d6150095bf6ca50c58ff4595ffc0f6fc536
dj-hirrot <hirotoshibutani@gmail.com>
2019-08-01 20:19:10 +0900

    [update] production/fixturesの追加

---
+++ db/fixtures/production/01_category.rb
@@ -0,0 +1,19 @@
+Category.seed do |c|
+  c.id = 1
+  c.name = 'DEVELOPMENT'
+end
+
+Category.seed do |c|
+  c.id = 2
+  c.name = 'HOBBY'
+end
+
+Category.seed do |c|
+  c.id = 3
+  c.name = 'PRIVATE'
+end
+
+Category.seed do |c|
+  c.id = 4
+  c.name = 'MONOSUMIDA'
+end
