05f401a03c787a273734984dd88e34510fc9d20e
dj-hirrot <hirotoshibutani@gmail.com>
2019-08-07 17:17:29 +0900

    [update] 権限の追加

--- db/fixtures/development/03_user_role.rb
+++ db/fixtures/development/03_user_role.rb
@@ -1,5 +1,7 @@
-UserRole.seed do |ur|
-  ur.id = 1
-  ur.user_id = 1
-  ur.role_id = 1
+3.times do |i|
+  UserRole.seed do |ur|
+    ur.id = i+1
+    ur.user_id = i+1
+    ur.role_id = i+1
+  end
 end
