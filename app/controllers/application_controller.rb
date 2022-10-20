d165ace88c6a15395d83324bf643e5eb5555cc4c
cc-hirrot <hr.shibutani@cloudcircus.co.jp>
2022-10-20 13:13:18 +0900

    fix: application_controllerにも追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
     def not_authenticated
       redirect_to new_session_path, alert: "Please login first"
     end
+
+    def admin_user?
+      if !(current_user.try(:admin?))
+        raise ActionController::RoutingError, with: :render_404
+      end
+    end
 end
