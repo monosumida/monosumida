80477bbe20e51602788b02c0021ec454c6a4c5c1
cc-hirrot <hr.shibutani@cloudcircus.co.jp>
2022-12-07 09:50:02 +0900

    updated

--- app/controllers/api/webhooks_controller.rb
+++ app/controllers/api/webhooks_controller.rb
@@ -1,12 +1,14 @@
 class Api::WebhooksController < Api::ApplicationController
   def create
     signature = request.headers['BowNow-Signature']
+    token = request.headers['BowNow-Request-Token']
     timestamp = request.headers['BowNow-Timestamp']
     api_key = '2cdd09505257d7d3bebe11c52f15a92a'
-    signed = Digest::SHA256.hexdigest("#{api_key}:#{timestamp}:#{request.body.read}")
+    signed = Digest::SHA256.hexdigest("#{api_key}:#{token}:#{request.body.read}")
     calced_headers = Base64.urlsafe_encode64(signed)
     render json: {
       result: request.body,
+      timestamp: timestamp,
       signature: signature,
       calced_headers: calced_headers
     }, status: :ok
