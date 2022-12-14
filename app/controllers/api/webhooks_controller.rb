6ee454ca654cd47d1f3a34cb8e78e5db6d9d4a6f
cc-hirrot <hr.shibutani@cloudcircus.co.jp>
2022-12-14 09:18:01 +0900

    change logic

--- app/controllers/api/webhooks_controller.rb
+++ app/controllers/api/webhooks_controller.rb
@@ -4,7 +4,7 @@ class Api::WebhooksController < Api::ApplicationController
     token = request.headers['BowNow-Request-Token']
     timestamp = request.headers['BowNow-Timestamp']
     api_key = '2cdd09505257d7d3bebe11c52f15a92a'
-    signed = Digest::SHA256.hexdigest("#{api_key}:#{token}:#{request.body.read}")
+    signed = Digest::SHA256.hexdigest("#{api_key}:#{timestamp}:#{token}:#{request.body.read}")
     calced_headers = Base64.urlsafe_encode64(signed)
     render json: {
       result: request.body,
