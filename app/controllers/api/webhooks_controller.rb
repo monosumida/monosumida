69142e0e8c4844ee114b0329d7a43b57766b805f
cc-hirrot <hr.shibutani@cloudcircus.co.jp>
2022-12-14 10:37:49 +0900

    updated

--- app/controllers/api/webhooks_controller.rb
+++ app/controllers/api/webhooks_controller.rb
@@ -4,13 +4,17 @@ class Api::WebhooksController < Api::ApplicationController
     token = request.headers['BowNow-Request-Token']
     timestamp = request.headers['BowNow-Timestamp']
     api_key = '2cdd09505257d7d3bebe11c52f15a92a'
+    Rails.logger.info('-----Request Body-----')
+    Rails.logger.info(request.body)
+    Rails.logger.info(request.body.read)
+    Rails.logger.info('-----Request Body-----')
     signed = Digest::SHA256.hexdigest("#{api_key}:#{timestamp}:#{token}:#{request.body.read}")
     calced_headers = Base64.urlsafe_encode64(signed)
     render json: {
-      result: request.body,
+      result: request.body.read,
       timestamp: timestamp,
-      signature: signature,
-      calced_headers: calced_headers
+      plain_signature: signature,
+      calc__signature: calced_headers
     }, status: :ok
   end
 end
